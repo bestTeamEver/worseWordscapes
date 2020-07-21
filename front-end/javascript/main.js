cfdf618c4dd08c79e81020cd6bdbd94a1daff54c
Luke Thompson <coachluke@gmail.com>
2020-07-21 13:54:37 -0700

    scoring function and validation implemented

--- front-end/javascript/main.js
+++ front-end/javascript/main.js
@@ -1,9 +1,47 @@
 // grab the current leaderboard and display it in #leaderboard
 document.getElementById("leaderboard").appendChild(getLeaderBoard());
 
-const characters = ["A", "B", "C", "D", "E", "F", "G", "H"];
+// frequency of each letter in english language.
+const letterFrequencies = {
+  E: 0.1202,
+  T: 0.091,
+  A: 0.0812,
+  O: 0.0768,
+  I: 0.0731,
+  N: 0.0695,
+  S: 0.0628,
+  R: 0.0602,
+  H: 0.0592,
+  D: 0.0432,
+  L: 0.0398,
+  U: 0.0288,
+  C: 0.0271,
+  M: 0.0261,
+  F: 0.023,
+  Y: 0.0211,
+  W: 0.0209,
+  G: 0.0203,
+  P: 0.0182,
+  B: 0.0149,
+  V: 0.0111,
+  K: 0.0069,
+  X: 0.0017,
+  Q: 0.0011,
+  J: 0.001,
+  Z: 0.0007,
+};
+
+// round stuff { ---------------------------------------------------------
+let characters = getRoundCharacters(6);
+let words;
+let usedWords = [];
+getWords(characters); // will assign to the variable 'words' at some point. see getWords()
+
 insertCharacters(characters);
 
+// }                -----------------------------------------------------
+
+// pulls the leaderboard data from db. Returns a filled out leaderboard HTML element.
 function getLeaderBoard() {
   // pull from a database somewhere.
 
@@ -22,6 +60,7 @@ function getLeaderBoard() {
   return createLeaderboardElement(entries);
 }
 
+// creates and returns a leaderboard HTML element. Accepts an array of objects with {name, score}
 function createLeaderboardElement(entries) {
   const leaderboard = document.createElement("ul");
   leaderboard.setAttribute("class", "list-group");
@@ -45,27 +84,67 @@ function createLeaderboardElement(entries) {
   return leaderboard;
 }
 
-// get the characters for this round
+// get the characters for this round. accepts a number for number of characters to use
+function getRoundCharacters(numCharacters) {
+  const characters = [];
 
-// load all the possible words into a data structure.
+  for (let i = 0; i < numCharacters; i++) {
+    characters.push(getValueFromLetterFreqs(Math.random()));
+  }
 
-// chose how many characters to use this round
+  return characters;
+}
+
+// finds the appropiate letter from the frequency table (requires number to be between 0 and 1, otherwise only return 'Z')
+function getValueFromLetterFreqs(num) {
+  let returning;
+  let sum = 0;
+  for (item in letterFrequencies) {
+    sum += letterFrequencies[item];
+    if (num < sum) {
+      return item;
+    }
+  }
+  return "Z"; // default return if doesn't work
+}
+
+// load all the possible words into a data structure. Takes in an array of characters, assigns
+// response from fetch call to the global 'words' variable.
+function getWords(characters) {
+  let query = characters.join("");
+
+  const response = fetch(
+    `https://word-scapes.herokuapp.com/start?letters=${query}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      words = data.filter((item) => item.length > 2);
+    });
+}
+
+// choose how many characters to use this round
 
 // update the current score
 function updateScore(increment) {
   const currentScore = document.querySelector("#current_score");
   let score = currentScore.innerHTML + increment;
-
   currentScore.innerHTML = score;
 }
 
-// check if word is valid
+// check if word is valid. Accepts a string. Returns true or false
 function isValidWord(word) {
-  // hasn't been used already
-  // is in the valid words array/object
-  // return true if good otherwise false
-}
+  if (typeof word === "string") {
+    if (
+      !usedWords.includes(word.toUpperCase()) &&
+      words.includes(word.toUpperCase())
+    ) {
+      return true;
 
+    }
+  }
+  // default return
+  return false;
+}
 
 // SCRABBLE SCORE CODE
 // assigns points to each letter of the alphabet
@@ -98,3 +177,20 @@ const letterVals = {
   "Z": 10
 }
 
+// get word, valiate, and score it
+function getWordScore(word) {
+  word = word.toUpperCase();
+  if(isValidWord(word)) {
+    usedWords.push(word);
+    words = words.filter(e => e !== word);
+    let wordScore = 0;
+  letterArray = word.split("");
+  for (let index = 0; index < letterArray.length; index++) {
+    const rawLetters = letterArray[index];
+    wordScore += letterVals[rawLetters] || 0;
+    }
+  return wordScore;
+  } else {
+  return 0;
+  }
+}
