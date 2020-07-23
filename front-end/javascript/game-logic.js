5a30c0694aa41021df5f74c5ef84908bb0c46487
Levi D Davis <levi.dd.davis@gmail.com>
2020-07-23 09:30:14 -0700

    updated backend calls to match new backend api

--- front-end/javascript/game-logic.js
+++ front-end/javascript/game-logic.js
@@ -8,69 +8,69 @@ let usedWords = [];
 //---------------------------------------Begin Character/Word Logic -------------------------
 
 // frequency of each letter in english language.
-const letterFrequencies = {
-  E: 0.1202,
-  T: 0.091,
-  A: 0.0812,
-  O: 0.0768,
-  I: 0.0731,
-  N: 0.0695,
-  S: 0.0628,
-  R: 0.0602,
-  H: 0.0592,
-  D: 0.0432,
-  L: 0.0398,
-  U: 0.0288,
-  C: 0.0271,
-  M: 0.0261,
-  F: 0.023,
-  Y: 0.0211,
-  W: 0.0209,
-  G: 0.0203,
-  P: 0.0182,
-  B: 0.0149,
-  V: 0.0111,
-  K: 0.0069,
-  X: 0.0017,
-  Q: 0.0011,
-  J: 0.001,
-  Z: 0.0007,
-};
+// const letterFrequencies = {
+//   E: 0.1202,
+//   T: 0.091,
+//   A: 0.0812,
+//   O: 0.0768,
+//   I: 0.0731,
+//   N: 0.0695,
+//   S: 0.0628,
+//   R: 0.0602,
+//   H: 0.0592,
+//   D: 0.0432,
+//   L: 0.0398,
+//   U: 0.0288,
+//   C: 0.0271,
+//   M: 0.0261,
+//   F: 0.023,
+//   Y: 0.0211,
+//   W: 0.0209,
+//   G: 0.0203,
+//   P: 0.0182,
+//   B: 0.0149,
+//   V: 0.0111,
+//   K: 0.0069,
+//   X: 0.0017,
+//   Q: 0.0011,
+//   J: 0.001,
+//   Z: 0.0007,
+// };
 
-// get the characters for this round. accepts a number for number of characters to use
-function getRoundCharacters(numCharacters) {
-  const characters = [];
+// // get the characters for this round. accepts a number for number of characters to use
+// function getRoundCharacters(numCharacters) {
+//   const characters = [];
 
-  for (let i = 0; i < numCharacters; i++) {
-    characters.push(getValueFromLetterFreqs(Math.random()));
-  }
+//   for (let i = 0; i < numCharacters; i++) {
+//     characters.push(getValueFromLetterFreqs(Math.random()));
+//   }
 
-  return characters;
-}
+//   return characters;
+// }
 
-// finds the appropiate letter from the frequency table (requires number to be between 0 and 1, otherwise only return 'Z')
-function getValueFromLetterFreqs(num) {
-  let sum = 0;
-  for (item in letterFrequencies) {
-    sum += letterFrequencies[item];
-    if (num < sum) {
-      return item;
-    }
-  }
-  return "Z"; // default return if doesn't work
-}
+// // finds the appropiate letter from the frequency table (requires number to be between 0 and 1, otherwise only return 'Z')
+// function getValueFromLetterFreqs(num) {
+//   let sum = 0;
+//   for (item in letterFrequencies) {
+//     sum += letterFrequencies[item];
+//     if (num < sum) {
+//       return item;
+//     }
+//   }
+//   return "Z"; // default return if doesn't work
+// }
 
 // load all the possible words into a data structure. Takes in an array of characters, assigns
 // response from fetch call to the global 'words' variable.
-function getWords(characters) {
-  let query = characters.join("");
-
+function getWords(numCharacters) {
   const response = fetch(
-    `https://word-scapes.herokuapp.com/start?letters=${query}`
+    `https://word-scapes.herokuapp.com/start?letters=${numCharacters}`
   )
     .then((response) => response.json())
     .then((data) => {
-      words = data.filter((item) => item.length > 2);
+      words = data.words.filter((item) => item.length > 2);
+      characters = data.letters;
+      insertCharacters(characters);
     });
 }
 
